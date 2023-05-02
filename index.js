0774bc06d3e7cdf576999371344c164ecd31e005
hasan <akonhasan680@gmail.com>
2023-05-03 00:18:13 +0600

    card details data set up

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const cardData=require('./cardData.json');
 app.get('/homeCard',(red,res)=>{
     res.send(cardData)
 })
-
+app.get('/:id',(req,res)=>{
+    const id=req.params.id;
+    const cardDetails=cardData.find(data=>data.id==id);
+    res.send(cardDetails);
+})
 
 app.listen(port,()=>{
     console.log('this server is running on port',{port})
